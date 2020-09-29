0426157cc781806d8d7a07af9236adb5d3af59e3
atsoystem <atosystem@hotmail.com>
2020-09-30 06:03:13 +0800

    combine basic function

--- python_webpage/app.py
+++ python_webpage/app.py
@@ -1,3 +1,28 @@
+import RPi.GPIO as GPIO    
+from time import sleep
+from RpiMotorLib import RpiMotorLib
+
+# Hardware settings
+GPIO.setmode(GPIO.BCM)
+
+GPIO_pins = (14, 15, 18)
+PIN_vertical_direction = 20
+PIN_vertical_step = 21
+
+PIN_horizontal_direction = 19
+PIN_horizontal_step = 26
+
+PIN_heat = 13 
+
+GPIO.setup(PIN_heat, GPIO.OUT)
+GPIO.output(PIN_heat, GPIO.LOW)
+
+motor_vertical = RpiMotorLib.A4988Nema(PIN_vertical_direction, PIN_vertical_step, GPIO_pins, "A4988")
+
+motor_horizontal = RpiMotorLib.A4988Nema(PIN_horizontal_direction, PIN_horizontal_step, GPIO_pins, "A4988")
+
+slider = 5
+
 from flask import Flask , send_file
 app = Flask(__name__)
 
@@ -22,10 +47,30 @@ def get_indexjs():
 def get_getStatus():
     return {"pos" : 2}
 
+@app.route('/turn1')
+def get_turn1():
+    motor_vertical.motor_go(True, "Full" , 600,int(slider)*.0004, False, .05)
+    return "ok"
+
+@app.route('/turn2')
+def get_turn2():
+    motor_horizontal.motor_go(True, "Full" , 600,int(slider)*.0004, False, .05)
+    return "ok"
+
+@app.route('/heaton')
+def get_heaton():
+    GPIO.output(PIN_heat, GPIO.HIGH)
+    return "ok"
+
+@app.route('/heatoff')
+def get_heatoff():
+    GPIO.output(PIN_heat, GPIO.LOW)
+    return "ok"
+
     # return "Hello World!"
 
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host= '0.0.0.0')
