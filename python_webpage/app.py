af8e28022724bd9370beebbed7b9254447e6665a
System Ato <atosystem@hotmail.com>
2020-10-11 02:06:17 +0800

    update code

--- python_webpage/app.py
+++ python_webpage/app.py
@@ -12,16 +12,156 @@ PIN_vertical_step = 21
 PIN_horizontal_direction = 19
 PIN_horizontal_step = 26
 
-PIN_heat = 13 
+PIN_heat = 13
+PIN_shoe_touch = 17
+
 
 GPIO.setup(PIN_heat, GPIO.OUT)
+GPIO.setup(PIN_shoe_touch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 GPIO.output(PIN_heat, GPIO.LOW)
+
 
 motor_vertical = RpiMotorLib.A4988Nema(PIN_vertical_direction, PIN_vertical_step, GPIO_pins, "A4988")
 
 motor_horizontal = RpiMotorLib.A4988Nema(PIN_horizontal_direction, PIN_horizontal_step, GPIO_pins, "A4988")
 
 slider = 5
+
+machine_status={
+    "status" : "idle",
+    "plate_state" : 0,
+    "plate_pos" : 0,
+    "temperature" : -1,
+    "humidity" : -1,
+    "shoes" : False
+}
+
+def initialize_machine():
+    print("Begin initialization")
+
+    # reset heat plate position
+    #TODO
+    # print("Reseting heat plate position")
+
+    # reset shoes plate position
+    #TODO
+    # print("Reseting shoes plate position")
+
+    # make sure shoes plate is not occupied
+    while(GPIO.input(PIN_shoe_touch)):
+        print("Please take out your shoes!",end='\r')
+        sleep(1)
+
+    print("Initialization done")
+    
+def getTempHumid():
+    # TODO
+    machine_status["temperature"] = 24
+    machine_status["humidity"] = 30
+    return (24,30)
+
+def operate_machine():
+    while(True):
+        machine_status={
+            "status" : "idle",
+            "plate_state" : 0,
+            "plate_pos" : 0,
+            "temperature" : -1,
+            "humidity" : -1,
+            "shoes" : False
+        }
+        
+        con = True
+
+        while(con):
+            # wait for shoes
+            while(not GPIO.input(PIN_shoe_touch)):
+                print("Waiting for shoes",end='\r')
+                sleep(1)
+            print("Shoes detected")
+            machine_status[status] = "shoes_detected"
+
+            sleep(2)
+            if GPIO.input(PIN_shoe_touch):
+                con = False
+            else:
+                con = True
+                machine_status[status] = "idle"
+
+        # move plate to state 1
+        # TODO
+        print("Moving plate to state 1")
+        print("[Done] Moving plate to state 1")
+
+        # detect if need dry
+        machine_status[status] = "pre_check"
+        machine_status[plate_state] = 1
+        print("Prechecking shoes")
+        print("[Done] Prechecking shoes")
+
+        getTempHumid()
+
+        need_dry = True
+
+        # shift down the shell
+        # TODO
+        machine_status[status] = "shelldown"
+        print("Shell down")
+        GPIO.output(PIN_heat, GPIO.HIGH)
+        print("On heat")
+        machine_status[status] = "heating"
+        while(need_dry):
+            
+            sleep(7)
+            getTempHumid()
+            
+            need_dry = False
+
+        print("off heat")
+        GPIO.output(PIN_heat, GPIO.LOW)
+
+        # shift up the shell
+        # TODO
+        machine_status[status] = "shell_up"
+        print("shell up")
+
+
+        # move plate to state 2
+        print("moving to state 2")
+        machine_status[status] = "done"
+        machine_status[state] = 2
+        print("done")
+
+        while(not GPIO.input(PIN_shoe_touch)):
+            print("Waiting for shoes to release",end='\r')
+            sleep(1)
+        print("Shoes release")
+        sleep(1)
+
+        # move the plate back to state 0
+        print("Moving plate back to state 0")
+        sleep(1)
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+    
+
+
+
+
 
 from flask import Flask , send_file
 app = Flask(__name__)
@@ -42,10 +182,15 @@ def get_jquery():
 def get_indexjs():
     return send_file("src/index.js")
 
+@app.route('/shoes.png')
+def get_shoes_png():
+    return send_file("src/shoes.png")
+
 
 @app.route('/getStatus')
 def get_getStatus():
-    return {"pos" : 2}
+    return machine_status
+
 
 @app.route('/turn1')
 def get_turn1():
@@ -73,4 +218,8 @@ def get_heatoff():
 
 
 if __name__ == '__main__':
+    initialize_machine()
     app.run(host= '0.0.0.0')
+    print("server start")
+    print("start machine")
+    operate_machine()
--- python_webpage/src/index.js
+++ python_webpage/src/index.js
@@ -2,15 +2,15 @@
 
 $( document ).ready(function() {
     
-    // let func_fetch_status = function fetch_status()
-    // {
+    let func_fetch_status = function fetch_status()
+    {
 
-    //     $.get("/getStatus", function(result){
-    //         console.log(result)
-    //         document.getElementById("div_status").innerHTML = result.pos
+        $.get("/getStatus", function(result){
+            console.log(result)
+            // document.getElementById("div_status").innerHTML = result.pos
 
-    //     });
-    // }
+        });
+    }
 
 
     setInterval(func_fetch_status,1000)
